30298f132d66f63a5834f8d4129fa3e80c55acf2
Raul Sanchez R. <raul.sr92@gmail.com>
2026-04-21 19:50:57 -0500

    Clase 69 : Pensemos en componentes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+//import { DashboardPage } from './gifs/pages/dashboard-page/dashboard-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'dashboard',
+    loadComponent: () => import('./gifs/pages/dashboard-page/dashboard-page'),
+  },
+  {
+    path: '**',
+    redirectTo:'dashboard',
+  }
+];
