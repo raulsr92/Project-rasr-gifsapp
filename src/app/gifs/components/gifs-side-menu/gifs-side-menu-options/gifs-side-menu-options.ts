8cfeeba3e4026575fcf4a0db8c2aebb7a12d8416
Raul Sanchez R. <raul.sr92@gmail.com>
2026-04-25 10:45:29 -0500

    Clase 71-Componentes para el menu lateral

---
+++ src/app/gifs/components/gifs-side-menu/gifs-side-menu-options/gifs-side-menu-options.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gifs-side-menu-options',
+  imports: [],
+  templateUrl: './gifs-side-menu-options.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class GifsSideMenuOptions { }
--- src/app/gifs/pages/dashboard-page/dashboard-page.ts
+++ src/app/gifs/pages/dashboard-page/dashboard-page.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { GifsSideMenu } from '../../components/gifs-side-menu/gifs-side-menu';
+
+
+
 
 @Component({
   selector: 'app-dashboard-page',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, GifsSideMenu],
   templateUrl: './dashboard-page.html',
 })
 export default class DashboardPage {
