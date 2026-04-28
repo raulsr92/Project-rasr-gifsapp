1c6145ad66150f35305d9ff6230fbfda8e01f39c
Raul Sanchez R. <raul.sr92@gmail.com>
2026-04-28 18:53:45 -0500

    Clase 72 - RouterLinks

--- src/app/gifs/components/gifs-side-menu/gifs-side-menu-options/gifs-side-menu-options.ts
+++ src/app/gifs/components/gifs-side-menu/gifs-side-menu-options/gifs-side-menu-options.ts
@@ -1,9 +1,35 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from "@angular/router";
+
+interface MenuOption {
+  label: string,
+  subLabel:string,
+  route: string,
+  icon: string
+}
 
 @Component({
   selector: 'app-gifs-side-menu-options',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './gifs-side-menu-options.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class GifsSideMenuOptions { }
+export class GifsSideMenuOptions {
+
+  menuOptions:MenuOption[] = [
+    {
+      label: 'Trending',
+      subLabel:'Gifs populares',
+      route:'/dashboard/trending',
+      icon: 'fa-solid fa-chart-line'
+    },
+    {
+      label: 'Buscador',
+      subLabel:'Buscar gifs',
+      route:'/dashboard/search',
+      icon: 'fa-solid fa-magnifying-glass'
+    }
+  ]
+
+
+ }
