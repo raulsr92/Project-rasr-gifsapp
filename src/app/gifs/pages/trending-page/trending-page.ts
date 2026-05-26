5a92a5b9ae1a417303ed66645f59f357304a0c97
Raul Sanchez R. <raul.sr92@gmail.com>
2026-05-26 16:48:21 -0500

    Clase 97 viewChild - tomar referencias del template

--- src/app/gifs/pages/trending-page/trending-page.ts
+++ src/app/gifs/pages/trending-page/trending-page.ts
@@ -1,7 +1,6 @@
-import {  Component, inject } from '@angular/core';
+import {  Component, ElementRef, inject, viewChild } from '@angular/core';
 import { GifsList } from '../../components/gifs-list/gifs-list';
 import { GifService } from '../../services/gifs.service';
-
 
 @Component({
   selector: 'app-trending-page',
@@ -11,11 +10,19 @@ import { GifService } from '../../services/gifs.service';
 export default class TrendingPage {
 
     //protected readonly imageUrls = imageUrls
+    //Referenciar elemento en html
 
+    scrollSectionRef = viewChild<ElementRef>('sectionScroll')
 
     //Inyectar servicio de petición http GET
-
       gifService = inject(GifService)
 
 
+    //Métodos
+
+      onScroll(event: Event){
+        const scrollSection = this.scrollSectionRef()?.nativeElement
+
+        console.log(scrollSection)
+      }
  }
